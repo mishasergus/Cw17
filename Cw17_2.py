b01df61c6661550aedd19e8e6f75586576c6cedc
mishasergus <sergusmisha@gmail.com>
2025-01-15 22:13:27 +0200

    commit1

---
+++ Cw17_2.py
@@ -0,0 +1,50 @@
+#469 574
+#145 588
+import time
+
+from PIL import ImageGrab, ImageOps
+import pyautogui
+from numpy import *
+class Dino:
+    def __init__(self, replaybtn, dino):
+        self.replaybtn = replaybtn
+        self.dino = dino
+        self.counter = 0
+        self.width = 55
+        self.grow = 1.0025
+
+    def restartgame(self):
+        pyautogui.click(self.replaybtn)
+        self.width = 55
+
+    def jump(self):
+        pyautogui.keyDown('space')
+        time.sleep(0.05)
+        pyautogui.keyUp('space')
+
+    def grabImage(self):
+        box = (self.dino[0]+self.width,self.dino[1],
+               self.dino[0]+40+self.width,self.dino[1]+30)
+        image = ImageGrab.grab(box)
+        grayImage = ImageOps.grayscale(image)
+        a = array(grayImage.getcolors())
+        return a.sum()
+
+    def start(self):
+        self.width *= self.grow
+        print(self.width)
+        if self.grabImage() != 1447:
+            self.jump()
+
+def main():
+    bot = Dino((469, 582), (145, 588))
+    bot.restartgame()
+    time.sleep(1)
+    bot.jump()
+    while True:
+        bot.start()
+        print(str(bot.grabImage()))
+
+
+if __name__ == "__main__":
+    main()
